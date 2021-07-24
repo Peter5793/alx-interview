e14c2254046584d49e952f80412572453f096de6
Peter Lugalia <p.lugalia@launchpad.alx.app>
2021-07-24 09:19:35 +0300

    fix pep8 error

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,15 +3,15 @@
 
 
 def pascal_triangle(n):
-  """ print triangel"""
+    """ Print triangle 1"""
     list_1 = []
     if n > 0:
-    for i in range(1, n + 1):
-        list_2 = []
-        last_num = 1
-        for j in range(1, i + 1):
-          list_2.append(last_num)
-        """ place binomial coeffiecient"""
-          last_num = last_num * (i - j) // j
-          list_1.append(list_2)
-    return list_1
+        for i in range(1, n + 1):
+            list_2 = []
+            last_num = 10
+            for j in range(1, i + 1):
+                list_2.append(last_num)
+                """ Place binomial co-effiecent"""
+                last_num = last_num * (i - j) // j
+                list_1.append(list_2)
+        return list_1

023c68ef0f00c2e0f2dab6f6b173115d280187fa
Peter5793 <masaba.peter68@gmail.com>
2021-07-24 09:29:13 +0300

    fix for loop

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,10 +8,10 @@ def pascal_triangle(n):
     if n > 0:
         for i in range(1, n + 1):
             list_2 = []
-            last_num = 10
+            last_num = 1
             for j in range(1, i + 1):
                 list_2.append(last_num)
                 """ Place binomial co-effiecent"""
                 last_num = last_num * (i - j) // j
-                list_1.append(list_2)
-        return list_1
+            list_1.append(list_2)
+    return list_1

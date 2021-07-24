d60d11d03572409ef29f431dc1ccbca9c78b1e86
Peter5793 <masaba.peter68@gmail.com>
2021-07-24 09:11:53 +0300

    fix pep8

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,14 +4,14 @@
 
 def pascal_triangle(n):
   """ print triangel"""
-  list_1 = []
-  if n > 0:
+    list_1 = []
+    if n > 0:
     for i in range(1, n + 1):
-      list_2 = []
-      last_num = 1
-      for j in range(1, i + 1):
-        list_2.append(last_num)
+        list_2 = []
+        last_num = 1
+        for j in range(1, i + 1):
+          list_2.append(last_num)
         """ place binomial coeffiecient"""
-        last_num = last_num * (i - j ) // j
-      list_1.append(list_2)
-  return list_1
+          last_num = last_num * (i - j) // j
+          list_1.append(list_2)
+    return list_1
